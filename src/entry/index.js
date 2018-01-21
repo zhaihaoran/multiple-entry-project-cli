ea3dffe93f97cb09eb7b566fbe886bde02c51391
zhaihaoran <359118915@qq.com>
2018-01-21 23:48:26 +0800

    sss

--- src/entry/index.js
+++ src/entry/index.js
@@ -7,18 +7,22 @@ import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
 import '@scss/index.scss';
 
-$('.animateNumber').prop('number', 0).animateNumber({
-    number: 9567,
-    easing: 'easeOut'
-}, 4000)
+$(document).ready(function(){
+    $('.animateNumber').prop('number', 0).animateNumber({
+        number: 9567,
+        easing: 'easeOut'
+    }, 4000)
 
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-});
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+    videojs('video', {
+        controls: true,
+        autoplay: false,
+        preload: 'auto'
+    });
+    $('.scrollspy').scrollSpy();
+})
 
-videojs('video', {
-    controls: true,
-    autoplay: false,
-    preload: 'auto'
-});
+
