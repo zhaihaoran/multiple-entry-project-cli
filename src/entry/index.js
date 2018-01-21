fb9cde343dd92880841fe075dd870fc6bc81b5eb
zhaihaoran <359118915@qq.com>
2018-01-21 16:21:50 +0800

    sss

--- src/entry/index.js
+++ src/entry/index.js
@@ -1,6 +1,7 @@
 import '@scss/index.scss';
 import $ from 'jquery';
 import '@lib/animateNumber'
+import 'materialize-css'
 
 console.log(moment().format());
 
@@ -8,3 +9,8 @@ $('#mmm').prop('number', 0).animateNumber({
     number: 9567,
     easing: 'easeOut'
 }, 4000)
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
