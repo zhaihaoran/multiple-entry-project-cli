2ae2c5cb0f0fd09f1565a51c0d622832563c4b46
zhaihaoran <359118915@qq.com>
2018-02-07 18:21:13 +0800

    sss

--- src/entry/video.js
+++ src/entry/video.js
@@ -1,6 +1,8 @@
 import M from 'materialize-css'
 import 'materialize-css/sass/materialize.scss'
 import _ from 'lodash'
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
 import '@scss/video.scss';
 
 class Video {
@@ -9,10 +11,33 @@ class Video {
     }
 
     initEvent() {
-        this.windowListenEvents();
+        this.declareVar()
+        this.pluginsInit()
+        this.videoPlay()
     }
 
-    windowListenEvents() {
+    declareVar() {
+        this.$videoPlay = $('.in-mask');
+        this.$tabs = $('ul.tabs');
+    }
+
+    videoPlay() {
+        let me = this;
+        this.$videoPlay.on('click', e => {
+            this.$videoPlay.addClass('hide').siblings('.index-mask').addClass('hide');
+            $('.video-player').removeClass('hide');
+            $('.video-player').find('#video').css('height', $('.index-video').height())
+            me.videoPlayer.play();
+        })
+    }
+
+    pluginsInit() {
+        this.$tabs.tabs();
+        this.videoPlayer = videojs('video', {
+            controls: true,
+            autoplay: false,
+            preload: 'auto'
+        });
     }
 }
 
